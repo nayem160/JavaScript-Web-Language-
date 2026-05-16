a3c59b7fdaffe6efcb5231aec40ddfd4ef56079f
Arafat Nayem <yanayem01@gmail.com>
2026-05-16 23:33:56 +0600

    Create setTimeout.js

---
+++ Advanced JavaScript/Asynchronous /setTimeout.js
@@ -0,0 +1,15 @@
+console.log("Dashbord");
+
+function loadProducts(){
+    
+    setTimeout(()=>{
+        const food = ["Kichuri", "bread"];
+
+    console.log(food)
+    }, 2000);
+    
+    console.log("Products Loaded successfully!");
+    
+}
+
+loadProducts();
